894c02412029c1c298d5b590ed1df3c7c17ea19f
Shevchik <Shev4ik.den@gmail.com>
2015-11-17 21:26:40 +0300

    This list is actually a list of all tile entities in the world. It is used in vanilla, but it is not used in spigot. But this list add() method needs to return true in order for server to function properly.

--- src/tickoptimizer/world/WorldInjector.java
+++ src/tickoptimizer/world/WorldInjector.java
@@ -4,6 +4,7 @@ import java.lang.invoke.MethodHandle;
 import java.util.ArrayList;
 
 import net.minecraft.server.v1_8_R3.Entity;
+import net.minecraft.server.v1_8_R3.TileEntity;
 import net.minecraft.server.v1_8_R3.World;
 
 import org.bukkit.Bukkit;
@@ -13,7 +14,7 @@ import tickoptimizer.utils.Utils;
 
 public class WorldInjector {
 
-	private final static MethodHandle toRemoveTileEntityListFieldSetter = Utils.getFieldSetter(World.class, "h", TileEntityCanUpdateSkipArrayList.class);
+	private final static MethodHandle loadedTileEntityListFieldSetter = Utils.getFieldSetter(World.class, "h", VoidList.class);
 	private final static MethodHandle pendingTileEntityListFieldSetter = Utils.getFieldSetter(World.class, "b", TileEntityCanUpdateSkipArrayList.class);
 	private final static MethodHandle tileEntityListFieldSetter = Utils.getFieldSetter(World.class, "tileEntityList", TileEntityCanUpdateSkipArrayList.class);
 	private final static MethodHandle entityListFieldSetter = Utils.getFieldSetter(World.class, "entityList", ArrayList.class);
@@ -22,7 +23,7 @@ public class WorldInjector {
 		try {
 			World nmsWorld = ((CraftWorld) world).getHandle();
 			entityListFieldSetter.invokeExact(nmsWorld, new ArrayList<Entity>());
-			toRemoveTileEntityListFieldSetter.invokeExact(nmsWorld, new TileEntityCanUpdateSkipArrayList());
+			loadedTileEntityListFieldSetter.invokeExact(nmsWorld, new VoidList<TileEntity>());
 			pendingTileEntityListFieldSetter.invokeExact(nmsWorld, new TileEntityCanUpdateSkipArrayList());
 			tileEntityListFieldSetter.invokeExact(nmsWorld, new TileEntityCanUpdateSkipArrayList());
 		} catch (Throwable t) {
