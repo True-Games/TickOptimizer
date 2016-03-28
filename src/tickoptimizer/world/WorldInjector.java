e7defb95f38f6ad14647b204ab938dbc236a67db
Shevchik <Shev4ik.den@gmail.com>
2016-03-28 19:44:26 +0300

    Use hashset fake list for tileEntityList

--- src/tickoptimizer/world/WorldInjector.java
+++ src/tickoptimizer/world/WorldInjector.java
@@ -8,6 +8,7 @@ import net.minecraft.server.v1_9_R1.Entity;
 import net.minecraft.server.v1_9_R1.EntityPlayer;
 import net.minecraft.server.v1_9_R1.MinecraftServer;
 import net.minecraft.server.v1_9_R1.PlayerChunkMap;
+import net.minecraft.server.v1_9_R1.TileEntity;
 import net.minecraft.server.v1_9_R1.World;
 import net.minecraft.server.v1_9_R1.WorldServer;
 
@@ -18,8 +19,9 @@ import tickoptimizer.utils.Utils;
 
 public class WorldInjector {
 
-	private final static MethodHandle tileEntityListFieldSetter = Utils.getFieldSetter(World.class, "tileEntityListTick", TileEntityCanUpdateSkipArrayList.class);
-	private final static MethodHandle pendingTileEntityListFieldSetter = Utils.getFieldSetter(World.class, "b", TileEntityCanUpdateSkipArrayList.class);
+	private final static MethodHandle tileEntityListFieldSetter = Utils.getFieldSetter(World.class, "tileEntityList", HashSetFakeListImpl.class);
+	private final static MethodHandle tileEntityListTickFieldSetter = Utils.getFieldSetter(World.class, "tileEntityListTick", TileEntityCanUpdateSkipArrayList.class);
+	private final static MethodHandle tileEntityListPendingFieldSetter = Utils.getFieldSetter(World.class, "b", TileEntityCanUpdateSkipArrayList.class);
 	private final static MethodHandle entityListFieldSetter = Utils.getFieldSetter(World.class, "entityList", ArrayList.class);
 	private final static MethodHandle managedPlayersPlayersFieldSetter = Utils.getFieldSetter(PlayerChunkMap.class, "managedPlayers", HashSetFakeListImpl.class);
 	private final static MethodHandle navigationListener = Utils.getFieldSetter(World.class, "t", OptimizedNavigationListener.class);
@@ -30,9 +32,10 @@ public class WorldInjector {
 		try {
 			WorldServer nmsWorldServer = ((CraftWorld) world).getHandle();
 			World nmsWorld = nmsWorldServer;
+			tileEntityListFieldSetter.invoke(nmsWorld, new HashSetFakeListImpl<TileEntity>());
+			tileEntityListPendingFieldSetter.invokeExact(nmsWorld, new TileEntityCanUpdateSkipArrayList());
+			tileEntityListTickFieldSetter.invokeExact(nmsWorld, new TileEntityCanUpdateSkipArrayList());
 			entityListFieldSetter.invokeExact(nmsWorld, new ArrayList<Entity>());
-			pendingTileEntityListFieldSetter.invokeExact(nmsWorld, new TileEntityCanUpdateSkipArrayList());
-			tileEntityListFieldSetter.invokeExact(nmsWorld, new TileEntityCanUpdateSkipArrayList());
 			navigationListener.invokeExact(nmsWorld, new OptimizedNavigationListener());
 			PlayerChunkMap chunkmap = nmsWorldServer.getPlayerChunkMap();
 			managedPlayersPlayersFieldSetter.invokeExact(chunkmap, new HashSetFakeListImpl<EntityPlayer>());
