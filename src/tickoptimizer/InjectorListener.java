e87ef8ed23dc1cd693c929fb9f3799001a90f586
Shevchik <Shev4ik.den@gmail.com>
2016-11-27 10:49:27 +0300

    Paper finally removed those fields

--- src/tickoptimizer/InjectorListener.java
+++ src/tickoptimizer/InjectorListener.java
@@ -3,10 +3,8 @@ package tickoptimizer;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.world.WorldInitEvent;
 
-import tickoptimizer.network.NetworkManagerInjector;
 import tickoptimizer.world.WorldInjector;
 
 public class InjectorListener implements Listener {
@@ -14,11 +12,6 @@ public class InjectorListener implements Listener {
 	@EventHandler(priority=EventPriority.LOWEST)
 	public void onWorldInit(WorldInitEvent event) {
 		WorldInjector.inject(event.getWorld());
-	}
-
-	@EventHandler(priority=EventPriority.LOWEST)
-	public void onPLayerJoin(PlayerJoinEvent event) {
-		NetworkManagerInjector.inject(event.getPlayer());
 	}
 
 }
